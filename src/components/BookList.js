fad510a9afeda536fc1b1c1e552d5545cf6c48c7
ahmetcan-7 <80100428+ahmetcan-7@users.noreply.github.com>
2021-04-10 19:42:22 +0300

    Add files via upload

---
+++ src/components/BookList.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import './BookList.css';
+import Book from './Book';
+import { BookContext } from '../context/BookContext';
+import { ThemeContext } from '../context/ThemeContext';
+
+class BookList extends React.Component {
+
+
+    render() {
+
+
+        return (
+            <ThemeContext.Consumer>
+                {contextTheme => (
+                    <BookContext.Consumer>
+                        {contextBook => {
+                            const { books } = contextBook;
+                            const { isDarkTheme, dark, light } = contextTheme;
+                            const theme = isDarkTheme ? dark : light;
+                            return (
+                                <section className="page-section " style={{ background: theme.bg, color: theme.txt }} id="portfolio">
+                                    <div className="container">
+                                        <div className="text-center">
+                                            <h2 className="section-heading text-uppercase">BookFolio</h2>
+                                            <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+                                        </div>
+                                        <div className="row">
+
+                                            {books.map((book, i) => {
+                                                return <Book book={book} key={i} />
+                                            })}
+
+                                        </div>
+                                    </div>
+                                </section>
+                            )
+                        }}
+                    </BookContext.Consumer>
+
+                )}
+
+            </ThemeContext.Consumer>
+        )
+    }
+
+}
+
+export default BookList;
